f4a76aecfe4ebb8ca82ce99476797bf324552953
Lumir-Ludvik <lumirludvik@email.cz>
2026-04-17 08:54:16 +0200

    add eslint and prettier and format components

--- App.tsx
+++ App.tsx
@@ -1,19 +1,19 @@
-import './src/global.css';
+import "./src/global.css";
 
-import React from 'react';
-import { useColorScheme } from 'react-native';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import React from "react";
+import { useColorScheme } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
 
-import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 
 export default function App() {
   const colorScheme = useColorScheme();
 
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <GluestackUIProvider mode={colorScheme === 'dark' ? 'dark' : 'light'}>
+    <GestureHandlerRootView className="flex-1">
+      <GluestackUIProvider mode={colorScheme === "dark" ? "dark" : "light"}>
         <View className="flex-1 items-center justify-center bg-background-0">
           <Text className="text-typography-900">
             Gluestack-UI v3 is ready. Open App.tsx to start.
